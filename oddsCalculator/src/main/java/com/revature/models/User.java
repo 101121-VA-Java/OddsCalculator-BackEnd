f3047f27f724d986f121ce06fb193483ed946b2e
tricialynn <plg1003@wildcats.unh.edu>
2021-12-08 15:58:59 -0500

    Added to the structure of the back end

---
+++ oddsCalculator/src/main/java/com/revature/models/User.java
@@ -0,0 +1,210 @@
+package com.revature.models;
+
+public class User {
+	private int userID;
+	private String firstName;
+	private String lastName;
+	private String password;
+	private String email;
+	private Role role;
+	private int wins;
+	private int losses;
+	
+	
+
+	public User() {
+	}
+
+
+
+	public User(int userID, String firstName, String lastName, String password, String email, Role role) {
+		super();
+		this.userID = userID;
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.password = password;
+		this.email = email;
+		this.role = role;
+	}
+
+
+
+	public User(int userID, String firstName, String lastName, String password, String email, Role role, int wins,
+			int losses) {
+		super();
+		this.userID = userID;
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.password = password;
+		this.email = email;
+		this.role = role;
+		this.wins = wins;
+		this.losses = losses;
+	}
+
+
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((email == null) ? 0 : email.hashCode());
+		result = prime * result + ((firstName == null) ? 0 : firstName.hashCode());
+		result = prime * result + ((lastName == null) ? 0 : lastName.hashCode());
+		result = prime * result + losses;
+		result = prime * result + ((password == null) ? 0 : password.hashCode());
+		result = prime * result + ((role == null) ? 0 : role.hashCode());
+		result = prime * result + userID;
+		result = prime * result + wins;
+		return result;
+	}
+
+
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		User other = (User) obj;
+		if (email == null) {
+			if (other.email != null)
+				return false;
+		} else if (!email.equals(other.email))
+			return false;
+		if (firstName == null) {
+			if (other.firstName != null)
+				return false;
+		} else if (!firstName.equals(other.firstName))
+			return false;
+		if (lastName == null) {
+			if (other.lastName != null)
+				return false;
+		} else if (!lastName.equals(other.lastName))
+			return false;
+		if (losses != other.losses)
+			return false;
+		if (password == null) {
+			if (other.password != null)
+				return false;
+		} else if (!password.equals(other.password))
+			return false;
+		if (role != other.role)
+			return false;
+		if (userID != other.userID)
+			return false;
+		if (wins != other.wins)
+			return false;
+		return true;
+	}
+
+
+
+	public int getUserID() {
+		return userID;
+	}
+
+
+
+	public void setUserID(int userID) {
+		this.userID = userID;
+	}
+
+
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+
+
+	public String getLastName() {
+		return lastName;
+	}
+
+
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+
+
+	public String getPassword() {
+		return password;
+	}
+
+
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+
+
+	public String getEmail() {
+		return email;
+	}
+
+
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+
+
+	public Role getRole() {
+		return role;
+	}
+
+
+
+	public void setRole(Role role) {
+		this.role = role;
+	}
+
+
+
+	public int getWins() {
+		return wins;
+	}
+
+
+
+	public void setWins(int wins) {
+		this.wins = wins;
+	}
+
+
+
+	public int getLosses() {
+		return losses;
+	}
+
+
+
+	public void setLosses(int losses) {
+		this.losses = losses;
+	}
+
+
+
+	@Override
+	public String toString() {
+		return "User [userID=" + userID + ", firstName=" + firstName + ", lastName=" + lastName + ", email=" + email
+				+ ", role=" + role + ", wins=" + wins + ", losses=" + losses + "]";
+	}
+	
+	
+
+}
