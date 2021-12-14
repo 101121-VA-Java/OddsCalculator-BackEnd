bd254ea9938f6789e0693c1f475e8444164efaa4
unknown <jpsteinhafel@gmail.com>
2021-12-14 14:21:36 -0800

    Adding Pair Recommendations Method

--- BlackJackAPI/src/main/java/com/revature/Services/RecommendationService.java
+++ BlackJackAPI/src/main/java/com/revature/Services/RecommendationService.java
@@ -22,15 +22,61 @@ public class RecommendationService {
 	public String pairRecommendation(String value, String DH) {
 		String rec = null;
 		if(value.equals("A") || value.equals("8")) {
-			rec = "Split";
+			rec = "Split ";
 		}
 		if(value.equals("10")) {
-			rec = "Do not split, stand";
+			//becomes a hard total -> needs to be passed into hardRecs
+			rec = "Do not split ";
 		}
-		if(value.equals("9")) {
-			rec = "Do not split, stand";
+		if( (value.equals("9") && DH.equals("7")) || (value.equals("9") && DH.equals("10")) || (value.equals("9") && DH.equals("A")) ) { 
+			//becomes a hard total -> needs to be passed into hardRecs
+			rec = "Do not split ";
+		} else if(value.equals("9")) { 
+			rec = "Split";
 		}
-		
+		if(value.equals("8")) { 
+			rec = "Split";
+		}
+		if( (value.equals("7") && DH.equals("8")) || (value.equals("7") && DH.equals("9")) || (value.equals("7") && DH.equals("10")) || (value.equals("7") && DH.equals("A")) ) { 
+			//becomes a hard total -> needs to be passed into hardRecs
+			rec = "Do not split ";
+		} else if(value.equals("7")) { 
+			rec = "Split";
+		}
+		if( (value.equals("6") && DH.equals("3")) || (value.equals("6") && DH.equals("4")) || (value.equals("6") && DH.equals("5")) ) { 
+			rec = "Split";
+		} else if (value.equals("6") && DH.equals("2")) {
+			//becomes a hard total -> needs to be passed into hardRecs
+			rec = "Split and double bet ";
+		} else if (value.equals("6")) {
+			//becomes a hard total -> needs to be passed into hardRecs
+			rec = "Do not split ";
+		}
+		if(value.equals("5")) { 
+			//becomes a hard total -> needs to be passed into hardRecs
+			rec = "Do not split ";
+		}
+		if( (value.equals("4") && DH.equals("5")) || (value.equals("4") && DH.equals("6")) ) {
+			//becomes a hard total -> needs to be passed into hardRecs
+			rec = "Split and double ";
+		} else if(value.equals("4")) {
+			//becomes a hard total -> needs to be passed into hardRecs
+			rec = "Do not split ";
+		}
+		if( (value.equals("3") && DH.equals("2")) || (value.equals("3") && DH.equals("3")) ) {
+			rec = "Split and double ";
+		} else if((value.equals("3") && DH.equals("4")) || (value.equals("3") && DH.equals("5")) || (value.equals("3") && DH.equals("6")) || (value.equals("3") && DH.equals("7")) ) {
+			rec = "Split ";
+		} else if(value.equals("3")) {
+			rec = "Do not split";
+		}
+		if( (value.equals("2") && DH.equals("2")) || (value.equals("2") && DH.equals("3")) ) {
+			rec = "Split and double ";
+		} else if((value.equals("2") && DH.equals("4")) || (value.equals("2") && DH.equals("5")) || (value.equals("2") && DH.equals("6")) || (value.equals("2") && DH.equals("7")) ) {
+			rec = "Split ";
+		} else if(value.equals("2")) {
+			rec = "Do not split ";
+		}
 		
 		return rec;
 	}
