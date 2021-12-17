3dde14fe8396fb9e91de02105b5bca64f257150d
tricialynn <plg1003@wildcats.unh.edu>
2021-12-17 15:46:18 -0500

    Full Update functionality added

--- src/main/java/com/revature/controllers/UserController.java
+++ src/main/java/com/revature/controllers/UserController.java
@@ -6,6 +6,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -39,22 +40,29 @@ public class UserController {
 		
 	}
 	
-//	@CrossOrigin
-//	@RequestMapping(method=RequestMethod.GET)
-////	@GetMapping(value="/{id}")
-//	public User getUsers(@RequestParam(name="id", required = true)int id){
-//		
-//		System.out.println("Getting User " + id);
-//		return us.getUser(id);
-//		
-//	}
+	@CrossOrigin
+	//@RequestMapping(method=RequestMethod.GET)
+	@GetMapping("/{id}")
+	public User getUsers(@PathVariable(name="id", required = true)int id){
+		
+		System.out.println("Getting User " + id);
+		return us.getUser(id);
+		
+	}
 	
 	@CrossOrigin
 	@RequestMapping(method=RequestMethod.POST)
 	public ResponseEntity<String> createUser(@RequestBody User user){
 		us.createUser(user);
-		System.out.println("We here!");
 		return new ResponseEntity<>(user.getFirstName() +" " + user.getLastName() + " was created.", HttpStatus.CREATED);
 	}
+	
+	@CrossOrigin
+	@PostMapping("/{id}/{field}")
+	public ResponseEntity<String> updateUser(@PathVariable(name="id", required = true)int id, @PathVariable(name="field", required=true) String field,@RequestBody User user){
+		us.updateUser(id, user, field);
+		return new ResponseEntity<>(user.getFirstName() +" " + user.getLastName() + " was updated.", HttpStatus.OK);
+	}
+	
 
 }
